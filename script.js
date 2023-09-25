6234b7f964104eb1a15e688b4a03737ff5d54b84
Markus Krabbi <markuskrabbi@gmail.com>
2023-09-25 11:17:03 +0300

    Add simple paint program

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-// console.log(window, {document});
-//     setTimeout(() => {
-//         document.location = 'https://google.com';
-//     }, 5000);
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
-ctx.strokeStyle = "black";
-ctx.strokeRect(40,20,320,200);
-ctx.fillStyle = 'blue';
-ctx.fillRect(40,20,320,66.6);
-ctx.beginPath();
-ctx.strokeRect(40,20,320,200);
-ctx.fillStyle = 'black';
-ctx.fillRect(40,86,320,66.6);
-ctx.closePath();
+//console.log(window, {document});
+// setTimeout(() => {
+//     document.location = 'https://google.com';
+// }, 5000);
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
 
+let x = 10;
+let y = 10;
+let pressed = false;
+let arcSize = 20;
+
+let color = document.querySelector('#color');
+
+setInterval(() => {
+    if(pressed){
+        ctx.fillStyle = color.value;
+        ctx.beginPath();
+        ctx.arc(x, y, arcSize, 0, 2*Math.PI);
+        ctx.fill();
+    }
+},16);
+
+canvas.addEventListener('mousemove', event => {
+    console.log(event);
+    x = event.clientX - canvas.offsetLeft;
+    y = event.clientY - canvas.offsetTop;
+});
+canvas.addEventListener('mousedown', event => {
+    pressed = true;
+});
+canvas.addEventListener('mouseup', event => {
+    pressed = false;
+});
+let size = document.querySelector('#size');
+let sizeValue = document.querySelector('#sizeValue');
+sizeValue.innerHTML = size.value;
+arcSize = size.value;
+size.addEventListener('input', ()=> {
+    sizeValue.innerHTML = size.value;
+    arcSize = size.value;
+});
 
