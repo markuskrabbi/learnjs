0f3d462ce3f2cfbadf1fa179c8dd0c0c51f0e419
Markus Krabbi <markuskrabbi@gmail.com>
2023-09-25 10:53:59 +0300

    Add Estonian flag

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 //     setTimeout(() => {
 //         document.location = 'https://google.com';
 //     }, 5000);
-let canvas = document.querySelector('canvas');
-let ctx = canvas.getContext('2d');
-ctx.moveTo(0,0);
-ctx.lineTo(200,100);
-ctx.stroke();
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+ctx.strokeStyle = "black";
+ctx.strokeRect(40,20,320,200);
+ctx.fillStyle = 'blue';
+ctx.fillRect(40,20,320,66.6);
 ctx.beginPath();
-ctx.arc( 100,100,50,0,2 * Math.PI);
-ctx.stroke();
-ctx.font = "30px Arial";
-ctx.fillText('Hello canvas', 30, 50);
-ctx.strokeText('Hello canvas', 30, 100);
-ctx.fillStyle = '#ff0';
-ctx.fillRect(10, 10, 10, 10);
+ctx.strokeRect(40,20,320,200);
+ctx.fillStyle = 'black';
+ctx.fillRect(40,86,320,66.6);
+ctx.closePath();
+
+
